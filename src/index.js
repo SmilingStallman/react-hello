28951d2a501b50283ff6e25e3768e096be7c0497
CryptoLain <klabweb@tuta.io>
2018-12-22 18:44:46 -0600

    Fix for sizing for mobile devices

--- src/index.js
+++ src/index.js
@@ -5,15 +5,24 @@ import AppGroup from './AppGroup';
 import PicArray from './PicArray';
 //import * as serviceWorker from './serviceWorker';
 
-//HelloWorld
-ReactDOM.render(
-  <div style={{display: "flex",
-              alignItems: "center",
-              justifyContent: "space-around"}}>
-    <AppGroup/>
-    <AppGroup/>
-    <AppGroup/>
-  </div>, document.getElementById('helloDiv'));
+
+//set HelloWorld with 1, 2, or 3 <AppGroup/> components based on viewport size
+function HelloWorld(){
+  if(document.documentElement.clientWidth <= 400)
+    return <div style={{display: "flex", alignItems: "center", justifyContent: "space-around"}}>
+      <AppGroup/></div>;
+
+  else if(document.documentElement.clientWidth <= 615)
+    return <div style={{display: "flex", alignItems: "center", justifyContent: "space-around"}}>
+      <AppGroup/><AppGroup/></div>;
+
+  else
+    return <div style={{display: "flex", alignItems: "center", justifyContent: "space-around"}}>
+      <AppGroup/><AppGroup/><AppGroup/></div>;
+}
+
+
+ReactDOM.render(HelloWorld(), document.getElementById('helloDiv'));
 
 //Trading
 ReactDOM.render(<PicArray/>, document.getElementById('pic-array'));
