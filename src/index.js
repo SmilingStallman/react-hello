d936519f7aced29b1c331b67fd6440bb9428b9ae
CryptoLain <klabweb@tuta.io>
2019-01-05 22:57:40 -0600

    Removed unusued celestial array component file

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import AppGroup from './AppGroup';
-import PicArray from './PicArray';
+import AppGroup from './AppGroup';;
 //import * as serviceWorker from './serviceWorker';
 
 
