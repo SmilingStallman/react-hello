38ec6a230f4330bad927f5859f08d8551a2fcd0c
CryptoLain <klabweb@tuta.io>
2019-01-16 14:21:21 -0600

    Update to styling

--- src/index.js
+++ src/index.js
@@ -4,20 +4,26 @@ import './index.css';
 import AppGroup from './AppGroup';;
 //import * as serviceWorker from './serviceWorker';
 
+const styles = {display: "flex",
+               alignItems: "center",
+               justifyContent: "space-around"};
 
 //set HelloWorld with 1, 2, or 3 <AppGroup/> components based on viewport size
 function HelloWorld(){
   if(document.documentElement.clientWidth <= 400)
-    return <div style={{display: "flex", alignItems: "center", justifyContent: "space-around"}}>
-      <AppGroup/></div>;
+    return <div style={styles}>
+            <AppGroup/>
+           </div>;
 
   else if(document.documentElement.clientWidth <= 615)
-    return <div style={{display: "flex", alignItems: "center", justifyContent: "space-around"}}>
-      <AppGroup/><AppGroup/></div>;
+    return <div style={styles}>
+            <AppGroup/><AppGroup/>
+          </div>;
 
   else
-    return <div style={{display: "flex", alignItems: "center", justifyContent: "space-around"}}>
-      <AppGroup/><AppGroup/><AppGroup/></div>;
+    return <div style={styles}>
+            <AppGroup/><AppGroup/><AppGroup/>
+           </div>;
 }
 ReactDOM.render(HelloWorld(), document.getElementById('helloDiv'));
 
