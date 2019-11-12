ce364eae63f5539a49549eb4dc903a92bb3b6ca6
miskellk <kyle.miskell@cdk.com>
2019-11-12 15:38:15 -0600

    server host package.json setup

---
+++ build/precache-manifest.a698c40b3688656a06e134f09b2a1c20.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e1cf92146217a5362be7f738fac2bec",
+    "url": "/react-hello/index.html"
+  },
+  {
+    "revision": "045ba4774a1c426f1e7d",
+    "url": "/react-hello/static/css/main.2cece063.chunk.css"
+  },
+  {
+    "revision": "af2e7e3a2c7e8da57e6a",
+    "url": "/react-hello/static/js/2.e696bd95.chunk.js"
+  },
+  {
+    "revision": "045ba4774a1c426f1e7d",
+    "url": "/react-hello/static/js/main.f3d598d8.chunk.js"
+  },
+  {
+    "revision": "4132b4b8b4b16fdf80b1",
+    "url": "/react-hello/static/js/runtime-main.2dfa5fce.js"
+  }
+]);
