de2719d385bbad316605a48de5e90b4b21863e67
SmilingStallman <klabweb@tuta.io>
2020-01-06 13:56:07 -0600

    domain name change for new unfoldkyle.com domain

---
+++ build/precache-manifest.d14672b9ae24d0fd9e2ab027029d3712.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3b6bb79f6fb3535e8fd3",
+    "url": "/react-hello/static/js/runtime~main.3b6bb79f.js"
+  },
+  {
+    "revision": "e2e2c2d51ef3a32db444",
+    "url": "/react-hello/static/js/main.e2e2c2d5.chunk.js"
+  },
+  {
+    "revision": "b023f85e4cd513ad074d",
+    "url": "/react-hello/static/js/1.b023f85e.chunk.js"
+  },
+  {
+    "revision": "e2e2c2d51ef3a32db444",
+    "url": "/react-hello/static/css/main.e0efcac6.chunk.css"
+  },
+  {
+    "revision": "1dff4fd18240000becf053adbaea82b7",
+    "url": "/react-hello/index.html"
+  }
+];
